445a3b7594eb133e38293ea9821a1a436cd28147
Nikita Labinskiy <nlabinskiy@gmail.com>
2022-10-16 17:06:42 +0300

    Шестая задача к первому семинару

---
+++ Task006/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число");
+
+int RundommNumber = Convert.ToInt32(Console.ReadLine());
+if (RundommNumber % 2 == 0)
+{
+    Console.Write("Четное");
+}
+else
+{
+    Console.Write("Нечетное");
+}
