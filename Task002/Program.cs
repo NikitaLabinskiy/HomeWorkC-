1a7c76bce6e081f8fa9ca3c96c4447c0049c6c5a
Nikita Labinskiy <nlabinskiy@gmail.com>
2022-10-16 16:57:19 +0300

    Вторая задача к первому семинару

---
+++ Task002/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+void NewArray(int numberA, int numberB)
+{
+    // Console.Write("Введите два числа: ");
+    // numberA = Convert.ToInt32(Console.ReadLine());
+    // numberB = Convert.ToInt32(Console.ReadLine());
+    if (numberA > numberB)
+    {
+        Console.Write("max = ");
+        Console.Write(numberA);
+    }
+    else
+    {
+        Console.Write("max = ");
+        Console.Write(numberB);
+    }
+}
+
+NewArray(4, 8);
