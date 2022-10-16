0fd24d4a4e895c314356b667285ad40e49f645f7
Nikita Labinskiy <nlabinskiy@gmail.com>
2022-10-16 17:10:24 +0300

    Восьмая задача к первому семинару

---
+++ Task008/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число");
+
+int RundommNumber = Convert.ToInt32(Console.ReadLine());
+ 
+int allEvenNumbers = 2;  //Начал с 2 чтобы сократить программу.
+
+while (allEvenNumbers <= RundommNumber)
+{
+    if (allEvenNumbers % 2 == 0)
+    {
+        Console.Write(allEvenNumbers);
+        Console.Write(", ");
+    }
+    allEvenNumbers+=2;
+}
