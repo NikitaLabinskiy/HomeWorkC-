01daa2e49d70a94d44cff9ed18b2f393c4e12949
Nikita Labinskiy <nlabinskiy@gmail.com>
2022-10-16 17:00:31 +0300

    Четвертая задача к первому семинару

---
+++ Task004/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+void NewArray(int[] numbers)
+{
+    int length = numbers.Length;
+    int index = 0;
+    int max = 0;
+    while (index < length)
+    {
+        if (numbers[index] > max)
+        {
+            max = numbers[index];
+        }
+        index++;
+    }
+    Console.Write("Max = ");
+    Console.Write(max);
+}
+
+int[] numbers = {1992, 372, 5699};
+
+NewArray(numbers);
