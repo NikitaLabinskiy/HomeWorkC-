853204ea86599cab27ea392440c4b2ea2b5a72a7
Nikita Labinskiy <nlabinskiy@gmail.com>
2022-10-21 20:41:29 +0300

    Домашняя работа к 2 семинару, задача 13

---
+++ Seminar2_Task13/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+
+int number = 37;
+
+if (number > 999 | number < 100 )
+{
+    while(number > 999 | number < 100 )
+    {
+        if(number > 999)
+        {
+            number /= 10;
+        }
+        if(number < 100)
+        {
+            Console.Write("Третьей цифры нет!");
+            break;
+        }
+        if (number < 999 & number >= 100)
+        {
+            Console.Write(number % 10);
+        }
+    }
+}
+else
+{
+    Console.Write(number % 10);
+}
