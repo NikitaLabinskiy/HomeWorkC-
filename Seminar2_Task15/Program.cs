39c36c6f967ea7f12f94983278d5eff4231678fe
Nikita Labinskiy <nlabinskiy@gmail.com>
2022-10-21 20:47:56 +0300

    Домашняя работа к 2 семинару, задача 15

---
+++ Seminar2_Task15/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+void Week (int number)
+{
+    bool flag = false;
+    int b = 6;
+    int a = 7;
+    if (number == a | number == b)
+    {
+        flag = true;
+    }
+    else
+    {
+        flag = false;
+    }
+    Console.Write(flag);
+}
+
+Console.Write("Введите число от 1 до 7: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Week(number1);
