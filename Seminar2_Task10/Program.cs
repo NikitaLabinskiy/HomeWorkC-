bf52350915eead8c12bf46663549bd59906b231b
Nikita Labinskiy <nlabinskiy@gmail.com>
2022-10-21 20:50:18 +0300

    Домашняя работа к 2 семинару, задача 10

---
+++ Seminar2_Task10/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+void ShowSecondDigit(int number)
+{
+    if(number > 100 & number < 1000)
+    {
+        int lastDigit = number / 10;
+        int middleDigit = number / 100;
+        int resault = lastDigit - middleDigit * 10;
+        Console.Write(resault);
+    }
+    else
+    {
+        Console.Write("Ошибка");
+    }
+}
+
+
+Console.Write("Введите трехзначное число ");
+
+int RundommNumber = Convert.ToInt32(Console.ReadLine());
+
+ShowSecondDigit(RundommNumber);
