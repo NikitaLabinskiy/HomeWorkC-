d4a8d4138ea95c6eac4d5fdf641a4d27e87de46f
Nikita Labinskiy <nlabinskiy@gmail.com>
2022-10-27 21:48:53 +0300

    Домашняя работа к 3 семинару, задача 23

---
+++ Seminar3_Task23/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int N);
+
+TableOfCubes(N);
+
+void TableOfCubes(int number)
+{
+    int[] array = new int[number + 1];
+    for(int i = 1; i <= number; i++)
+    {
+        array[i] = (i * i) * i;
+        Console.Write($"{array[i]} ");
+    }
+}
