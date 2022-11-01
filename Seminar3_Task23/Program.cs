c8b4d6f4e96eb5a7ddd058b81ce40339dbd09a44
Nikita Labinskiy <nlabinskiy@gmail.com>
2022-11-01 22:03:32 +0300

    Домашняя работа к 3 семинару, задача 23 new

--- Seminar3_Task23/Program.cs
+++ Seminar3_Task23/Program.cs
@@ -1,18 +1,22 @@
-﻿Console.Clear();
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N. 
+Задание должно быть выполнено в методе и метод должен вернуть массив чисел.
+3 -> 1, 8, 27; 5 -> 1, 8, 27, 64, 125*/
+
+Console.Clear();
 
 Console.Write("Введите число: ");
 bool isParsedN = int.TryParse(Console.ReadLine(), out int N);
 
-TableOfCubes(N);
+PrintArray(TableOfCubes(N));
 
-void TableOfCubes(int number)
+int[] TableOfCubes(int number)
 {
     int[] array = new int[number + 1];
     for(int i = 1; i <= number; i++)
     {
         array[i] = (i * i) * i;
     }
-    PrintArray(array);
+    return array;
 }
 
 void PrintArray(int[] array)
