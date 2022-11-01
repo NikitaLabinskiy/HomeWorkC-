ad9abcd0f5e648f864c039f0ea5b47c779e22cad
Nikita Labinskiy <nlabinskiy@gmail.com>
2022-11-01 22:06:36 +0300

    Домашняя работа к 3 семинару, задача 21 new

--- Seminar3_Task21/Program.cs
+++ Seminar3_Task21/Program.cs
@@ -24,7 +24,6 @@ Console.Write(final);
 double DistanceBetweenPoints(double x1, double y1, double z1, double x2, double y2, double z2)
 {
     double result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
-    // Console.WriteLine($"Расстояние между точками {result}");
 
     return result;
 }
